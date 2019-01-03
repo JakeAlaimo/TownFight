9c3641e1b292442e406ecdad53888dd22408f498
Jake Alaimo <jacob.alaimo@gmail.com>
2019-01-03 14:35:04 -0500

    Points the export directory to the correct module folder

--- Module_system 1.171/module_info.py
+++ Module_system 1.171/module_info.py
@@ -2,5 +2,5 @@
 # Point export_dir to the folder you will be keeping your module
 # Make sure you use forward slashes (/) and NOT backward slashes (\)
 
-export_dir = "Change me to your module's folder!"
+export_dir = "C:/Anything Somewhat Game Related/Non-Steam Games/Mount&Blade Warband/Modules/ScriptTest"
 #export_dir = "C:/Program Files (x86)/Mount&Blade Warband/Modules/Native/"
